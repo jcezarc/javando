267d841423e78ba9ba23cc52fdc424c17cf52d98
Júlio Cascalles <jcezarc@yahoo.com.br>
2022-04-27 16:03:01 -0300

    Enfatizando os `type hints` das funções

--- __main__.py
+++ __main__.py
@@ -3,10 +3,10 @@ import v1
 import v2
 from lista_nomes import LISTA_DE_NOMES
 
-print(v1.nome_bebê(random, 'masculino', LISTA_DE_NOMES))
-print(v1.nome_bebê(random, 'feminino', LISTA_DE_NOMES))
-print(v1.nome_bebê(random, 'neutro', LISTA_DE_NOMES))
+print(v1.nome_bebê(choice, 'masculino', LISTA_DE_NOMES))
+print(v1.nome_bebê(choice, 'feminino', LISTA_DE_NOMES))
+print(v1.nome_bebê(choice, 'neutro', LISTA_DE_NOMES))
 print('-'*30)
-print(v2.nome_bebê(random, 'masculino', LISTA_DE_NOMES))
-print(v2.nome_bebê(random, 'feminino', LISTA_DE_NOMES))
-print(v2.nome_bebê(random, 'neutro', LISTA_DE_NOMES))
+print(v2.nome_bebê(choice, 'masculino', LISTA_DE_NOMES))
+print(v2.nome_bebê(choice, 'feminino', LISTA_DE_NOMES))
+print(v2.nome_bebê(choice, 'neutro', LISTA_DE_NOMES))
